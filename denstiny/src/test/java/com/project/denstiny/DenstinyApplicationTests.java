09b274a250794c43a34e4c0a5bd5116f143213a8
yys <dgh06008@naver.com>
2024-07-13 18:22:21 +0900

    feat: module 생성

---
+++ denstiny/src/test/java/com/project/denstiny/DenstinyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.denstiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenstinyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
