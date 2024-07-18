2dc9076aef1c9b88756d989e6a481654e3b94604
yys <dgh06008@naver.com>
2024-07-18 10:41:29 +0900

    feat: spring security 설정 파일 추가

---
+++ denstiny/main-server/src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.example.config;
+
+import com.example.user.enums.UserRole;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf((auth) -> auth.disable())
+                .formLogin((auth) -> auth.disable())
+                .httpBasic((auth) -> auth.disable())
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/open-api/users/login", "/open-api/users/register", "/").permitAll()
+                        .requestMatchers("/admin").hasRole(String.valueOf(UserRole.ADMIN))
+                        .anyRequest().authenticated())
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // jwt 방식은 session 을 stateless 한 상태로 관리
+
+        return httpSecurity.build();
+    }
+
+}
