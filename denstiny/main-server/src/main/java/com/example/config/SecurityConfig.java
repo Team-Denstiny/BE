932d53d250d07eef125cf30b1dedbd4fd59dfc27
yys <dgh06008@naver.com>
2024-07-18 17:07:18 +0900

    feat: custom filter 추가

--- denstiny/main-server/src/main/java/com/example/config/SecurityConfig.java
+++ denstiny/main-server/src/main/java/com/example/config/SecurityConfig.java
@@ -1,17 +1,34 @@
 package com.example.config;
 
+import com.example.jwt.JWTFilter;
+import com.example.jwt.JWTUtil;
+import com.example.jwt.LoginFilter;
 import com.example.user.enums.UserRole;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
@@ -28,6 +45,8 @@ public class SecurityConfig {
                         .requestMatchers("/open-api/users/login", "/open-api/users/register", "/").permitAll()
                         .requestMatchers("/admin").hasRole(String.valueOf(UserRole.ADMIN))
                         .anyRequest().authenticated())
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class)
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration),jwtUtil), UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // jwt 방식은 session 을 stateless 한 상태로 관리
 
