c591d4ffe241a6bc29714f90590f5b9d80ada580
yys <dgh06008@naver.com>
2024-08-05 16:02:38 +0900

    feat: oauth2 에서 받지 못하는 주소 정보 추가 입력 endpoint
    endpoint location :  /jwt <-> /oauth2/endpoint

---
+++ denstiny/main-server/src/main/java/com/example/oauth2/dto/Oauth2AddressPlusDto.java
@@ -0,0 +1,10 @@
+package com.example.oauth2.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Oauth2AddressPlusDto {
+    private String address;
+}
--- denstiny/main-server/src/main/java/com/example/oauth2/endpoint/ResendTokenEndPoint.java
+++ denstiny/main-server/src/main/java/com/example/oauth2/endpoint/ResendTokenEndPoint.java
@@ -1,13 +1,17 @@
-package com.example.jwt;
+package com.example.oauth2.endpoint;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import com.example.jwt.JWTUtil;
+import com.example.oauth2.dto.Oauth2AddressPlusDto;
+import com.example.user.UserEntity;
+import com.example.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,9 +19,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import static com.example.constant.TokenHeaderConstant.HEADER_AUTHORIZATION;
 import static com.example.constant.TokenHeaderConstant.TOKEN_PREFIX;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/login")
 public class ResendTokenEndPoint {
+
+    private final UserRepository userRepository;
+    private final JWTUtil jwtUtil;
 
     /* 
     @GetMapping("/endpoint")
@@ -35,28 +44,39 @@ public class ResendTokenEndPoint {
     }
         */
 
-@GetMapping("/endpoint")
-public ResponseEntity<Map<String, String>> getCookieData(HttpServletRequest request) {
-    System.out.println("Get into!!!");
+    @PostMapping("/endpoint")
+    public ResponseEntity<Map<String, Long>> getCookieData(
+            HttpServletRequest request,
+            @RequestBody Oauth2AddressPlusDto oauth2AddressPlusDto
+    ) {
 
-    Cookie[] cookies = request.getCookies();
-    HttpHeaders headers = new HttpHeaders();
-    Map<String, String> response = new HashMap<>();
-    
-    if (cookies != null) {
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("access")) {
-                String cookieValue = cookie.getValue();
-                System.out.println(cookieValue);
-                headers.set(HEADER_AUTHORIZATION, TOKEN_PREFIX + cookieValue);
-                response.put("cookieData", cookieValue); // 응답에 cookieData 포함
+
+        Cookie[] cookies = request.getCookies();
+        HttpHeaders headers = new HttpHeaders();
+        Map<String, Long> response = new HashMap<>();
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("access")) {
+                    String cookieValue = cookie.getValue();
+
+                    String resourceId = jwtUtil.getResourceId(cookieValue);
+
+                    log.info("resourceId : {}",resourceId);
+
+                    UserEntity byResourceId = userRepository.findByResourceId(resourceId);
+                    Long userId = byResourceId.getUserId();
+
+
+                    headers.set(HEADER_AUTHORIZATION, TOKEN_PREFIX + cookieValue);
+                    response.put("id", userId);
+                }
             }
         }
-    }
-    
-    return ResponseEntity.ok()
-            .headers(headers)
-            .body(response); // JSON 응답으로 반환
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(response); // JSON 응답으로 반환
     }
 }
 
