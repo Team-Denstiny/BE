96fc0222a63e120cf3457ef967a691c366e261c0
junhyeong <baejhmath@soongsil.ac.kr>
2024-08-05 21:20:24 +0900

    temp

--- denstiny/main-server/src/main/java/com/example/jwt/ResendTokenEndPoint.java
+++ denstiny/main-server/src/main/java/com/example/jwt/ResendTokenEndPoint.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,44 +20,45 @@ import jakarta.servlet.http.HttpServletRequest;
 @RequestMapping("/login")
 public class ResendTokenEndPoint {
 
-    /* 
+    /*
+     * @GetMapping("/endpoint")
+     * public Map<String, String> handleRequest(@RequestHeader Map<String, String>
+     * headers) {
+     * // 로그에 요청 헤더를 출력합니다.
+     * System.out.println("Request received at /login/endpoint");
+     * for (Map.Entry<String, String> entry : headers.entrySet()) {
+     * System.out.println(entry.getKey() + ": " + entry.getValue());
+     * }
+     * 
+     * // 간단한 응답을 반환합니다.
+     * Map<String, String> response = new HashMap<>();
+     * response.put("message", "Request received and headers logged.");
+     * return response;
+     * }
+     */
+    @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
     @GetMapping("/endpoint")
-    public Map<String, String> handleRequest(@RequestHeader Map<String, String> headers) {
-        // 로그에 요청 헤더를 출력합니다.
-        System.out.println("Request received at /login/endpoint");
-        for (Map.Entry<String, String> entry : headers.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
+    public ResponseEntity<Map<String, String>> getCookieData(HttpServletRequest request) {
+        System.out.println("Get into!!!");
 
-        // 간단한 응답을 반환합니다.
+        Cookie[] cookies = request.getCookies();
+        HttpHeaders headers = new HttpHeaders();
         Map<String, String> response = new HashMap<>();
-        response.put("message", "Request received and headers logged.");
-        return response;
-    }
-        */
 
-@GetMapping("/endpoint")
-public ResponseEntity<Map<String, String>> getCookieData(HttpServletRequest request) {
-    System.out.println("Get into!!!");
-
-    Cookie[] cookies = request.getCookies();
-    HttpHeaders headers = new HttpHeaders();
-    Map<String, String> response = new HashMap<>();
-    
-    if (cookies != null) {
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("access")) {
-                String cookieValue = cookie.getValue();
-                System.out.println(cookieValue);
-                headers.set(HEADER_AUTHORIZATION, TOKEN_PREFIX + cookieValue);
-                response.put("cookieData", cookieValue); // 응답에 cookieData 포함
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("access")) {
+                    String cookieValue = cookie.getValue();
+                    System.out.println(cookieValue);
+                    headers.set(HEADER_AUTHORIZATION, TOKEN_PREFIX + cookieValue);
+                    response.put("cookieData", cookieValue); // 응답에 cookieData 포함
+                }
             }
         }
-    }
-    
-    return ResponseEntity.ok()
-            .headers(headers)
-            .body(response); // JSON 응답으로 반환
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(response); // JSON 응답으로 반환
     }
 }
 
