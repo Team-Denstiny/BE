f00e00df4a97eab3417b656619c4b18c8ded15d5
yys <dgh06008@naver.com>
2024-08-08 21:26:22 +0900

    feat: objectmapper time 형식 추가

--- denstiny/main-server/src/main/java/com/example/config/JacksonConfiguration.java
+++ denstiny/main-server/src/main/java/com/example/config/JacksonConfiguration.java
@@ -1,9 +1,14 @@
 package com.example.config;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalTime;
 
 @Configuration
 public class JacksonConfiguration {
@@ -11,5 +16,15 @@ public class JacksonConfiguration {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer customizeJackson() {
         return builder -> builder.propertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+    }
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        objectMapper.registerModule(javaTimeModule);
+        // 기본 포맷을 설정할 수 있습니다.
+        objectMapper.configOverride(LocalTime.class)
+                .setFormat(JsonFormat.Value.forPattern("HH:mm:ss"));
+        return objectMapper;
     }
 }
