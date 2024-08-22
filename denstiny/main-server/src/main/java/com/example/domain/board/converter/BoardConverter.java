22c348bea24a0ebc0e6df4ed15b965728e10fd28
tjdbwns19 <tjdbwns19@naver.com>
2024-08-22 12:39:33 +0900

    feat: 게시물 작성 Converter 생성
    toBoardEntity: BoardRequest -> BoardEntity
    toBoardResponse: BoardEntity -> BoardResponse

---
+++ denstiny/main-server/src/main/java/com/example/domain/board/converter/BoardConverter.java
@@ -0,0 +1,36 @@
+package com.example.domain.board.converter;
+
+import annotation.Converter;
+import java.util.Optional;
+
+import com.example.board.BoardEntity;
+import com.example.domain.board.controller.model.BoardRequest;
+import com.example.domain.board.controller.model.BoardResponse;
+import error.ErrorCode;
+import exception.ApiException;
+
+
+@Converter
+public class BoardConverter {
+    public BoardEntity toBoardEntity(BoardRequest request, Long userId){
+        return Optional.ofNullable(request)
+                .map(it -> {
+                    return BoardEntity.builder()
+                            .title(request.getTitle())
+                            .content(request.getContent())
+                            .writer(userId)
+                            .build();
+                })
+                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "BoardRequest is null"));
+    }
+
+    public BoardResponse toBoardResponse(BoardEntity board){
+        return Optional.ofNullable(board)
+                .map(it -> {
+                    return BoardResponse.builder()
+                            .boardId(it.getId())
+                            .writer(it.getWriter())
+                            .build();
+                }).orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "BoardEntity is null"));
+    }
+}
