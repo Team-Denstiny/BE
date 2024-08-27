4eb3bc1852f8e8f60b0c6aeeee93343a83bb0f8c
yys <dgh06008@naver.com>
2024-08-27 15:24:54 +0900

    fix: util class 위도,경도 순으로 입력 받도록 수정

--- denstiny/main-server/src/main/java/com/example/util/DistanceUtil.java
+++ denstiny/main-server/src/main/java/com/example/util/DistanceUtil.java
@@ -3,7 +3,7 @@ public class DistanceUtil {
 
     private static final int EARTH_RADIUS_KM = 6371;
 
-    public static double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    public static double calculateDistance(double lon1, double lat1, double lon2, double lat2) {
         double latDistance = Math.toRadians(lat2 - lat1);
         double lonDistance = Math.toRadians(lon2 - lon1);
         double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
