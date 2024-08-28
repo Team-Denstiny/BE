04663a733891f1b19938c189d27f69e8b2443b46
tjdbwns19 <tjdbwns19@naver.com>
2024-08-28 10:59:27 +0900

    refactor: 게시물 작성 RequestDto 필드 변경
    * @RequestPart로 List<MultipartFile>, BoardRequest 나눠서 받으므로 BoardRequest에서 이미지 리스트 필드 제거

--- denstiny/main-server/src/main/java/com/example/domain/board/controller/model/BoardRequest.java
+++ denstiny/main-server/src/main/java/com/example/domain/board/controller/model/BoardRequest.java
@@ -4,13 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class BoardRequest {
     private String title;
     private String content;
-    private List<String> pictures;
+    private Integer category;
 }
