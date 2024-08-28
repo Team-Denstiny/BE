3f1ad60ff0a969a2ece286ed72eca80cb772fa5a
tjdbwns19 <tjdbwns19@naver.com>
2024-08-28 11:23:40 +0900

    refactor: 게시물 Repository ID 타입 변경
    * long -> Long으로 수정

--- denstiny/sql-db/src/main/java/com/example/board/BoardRepository.java
+++ denstiny/sql-db/src/main/java/com/example/board/BoardRepository.java
@@ -2,6 +2,6 @@ package com.example.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardRepository extends JpaRepository<BoardEntity,Long> {
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
 
 }
