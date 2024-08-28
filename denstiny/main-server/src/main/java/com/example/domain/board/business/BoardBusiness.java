3b28a9368ba3cea6d0c2cb1c968867368fe3f210
tjdbwns19 <tjdbwns19@naver.com>
2024-08-28 10:56:10 +0900

    feat: 게시물 Buisness 작성 부분 사진 파일 업로드 기능 추가
    * board 테이블(부모) - board_image 테이블(자식) 연관관계로 인해 board 객체 생성 후, board_image 생성
    * ImageUtil로 업로드한 이미지 파일 정보 변경
    * BoardResponse로 응답

--- denstiny/main-server/src/main/java/com/example/domain/board/business/BoardBusiness.java
+++ denstiny/main-server/src/main/java/com/example/domain/board/business/BoardBusiness.java
@@ -1,29 +1,59 @@
 package com.example.domain.board.business;
 
 import annotation.Business;
+import api.Api;
+import com.example.board.BoardEntity;
+import com.example.board.BoardImageEntity;
+import com.example.domain.board.service.BoardImageService;
 import com.example.domain.board.service.BoardService;
 import com.example.domain.board.converter.BoardConverter;
+import com.example.util.ImageUtil;
 import com.example.domain.board.controller.model.BoardRequest;
 import com.example.domain.board.controller.model.BoardResponse;
 import error.ErrorCode;
 import exception.ApiException;
 import lombok.AllArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
+import java.util.List;
+import java.util.ArrayList;
 
 
 @Business
 @AllArgsConstructor
 public class BoardBusiness {
     private final BoardService boardService;
+    private final BoardImageService boardImageService;
     private final BoardConverter boardConverter;
 
-    public BoardResponse addBoard(BoardRequest boardRequest, Long userId){
+    public BoardResponse addBoard(BoardRequest boardRequest, List<MultipartFile> images, Long userId){
 
-        return Optional.ofNullable(boardRequest)
+        BoardEntity board = Optional.ofNullable(boardRequest)
                 .map(it -> boardConverter.toBoardEntity(it, userId))
                 .map(it -> boardService.addBoard(it))
-                .map(it -> boardConverter.toBoardResponse(it))
                 .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "addBoard null point"));
+
+        // findById 시 Optional 처리
+        // BoardEntity boardId = boardService.getReferenceBoardId(board.getBoardId()).orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "Board Id null point"));
+
+        try {
+            List<BoardImageEntity> list = ImageUtil.parseFileInfo(boardService.getReferenceBoardId(board.getBoardId()), images);
+
+            if (list.isEmpty()){
+            }
+            else{
+                List<BoardImageEntity> pictureBeans = new ArrayList<>();
+                for (BoardImageEntity boardImage : list) {
+                    pictureBeans.add(boardImageService.addBoardImage(boardImage));
+                }
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return boardConverter.toBoardResponse(board);
     }
 }
