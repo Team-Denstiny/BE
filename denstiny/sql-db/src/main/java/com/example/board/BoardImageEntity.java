9f7b49f13578fe1bcd3c0ca5a5db0b6ce5cc340b
tjdbwns19 <tjdbwns19@naver.com>
2024-08-28 11:21:16 +0900

    feat: 게시물 이미지 Entity 생성
    * ImageUtil에서 나노초 + 파일 형식 으로 storedFileName 고유 값으로 설정하였으므로 PK로 사용
    * board 테이블과 N : 1 객체 연관관계 매핑을 위해 @ManyToOne, @JoinColumn 어노테이션 적용
    * BoardImage 테이블에 저장시 Select -> Insert 쿼리를 Insert로만 처리하기 위해 Persistable<String> - isNew() 메서드 오버라이드

---
+++ denstiny/sql-db/src/main/java/com/example/board/BoardImageEntity.java
@@ -0,0 +1,42 @@
+package com.example.board;
+
+import com.example.base.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.domain.Persistable;
+
+@Entity
+@Data
+@Table(name = "board_image")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BoardImageEntity extends BaseEntity implements Persistable<String> {
+    @Id
+    @Column(name = "stored_file_name")
+    private String storedFileName;
+
+    @Column(name = "original_file_name")
+    private String originalFileName;
+
+    @Column(name = "file_size")
+    private Long fileSize;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private BoardEntity board;
+
+    @Override
+    public String getId() {
+        return storedFileName;
+    }
+
+    @Override
+    public boolean isNew() {
+        return getCreated_at() == null;
+    }
+}
