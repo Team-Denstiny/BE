a015a94f446810e75b9abfa6cccc33413161270e
tjdbwns19 <tjdbwns19@naver.com>
2024-09-02 22:12:04 +0900

    feat: 좋아요 Repository 생성
    * findByUserAndBoard: user, board 객체로 Heart 데이터 조회

---
+++ denstiny/sql-db/src/main/java/com/example/heart/HeartRepository.java
@@ -0,0 +1,11 @@
+package com.example.heart;
+
+import com.example.board.BoardEntity;
+import com.example.user.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface HeartRepository extends JpaRepository<HeartEntity, Long> {
+    Optional<HeartEntity> findByUserAndBoard(UserEntity user, BoardEntity board);
+}
