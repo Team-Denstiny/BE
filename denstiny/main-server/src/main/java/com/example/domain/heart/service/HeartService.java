2cea58eff649bdf274b829813a954da1e4d1281f
tjdbwns19 <tjdbwns19@naver.com>
2024-09-02 21:52:33 +0900

    feat: 좋아요 Service 생성
    * findByUserAndBoard: user, board 프록시 객체로 좋아요 조회
    * addHeart: 좋아요 생성
    * deleteHeart: 좋아요 삭제

---
+++ denstiny/main-server/src/main/java/com/example/domain/heart/service/HeartService.java
@@ -0,0 +1,34 @@
+package com.example.domain.heart.service;
+
+import com.example.board.BoardEntity;
+import com.example.heart.HeartEntity;
+import com.example.heart.HeartRepository;
+import com.example.user.UserEntity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class HeartService {
+    private final HeartRepository heartRepository;
+
+    public Optional<HeartEntity> findByUserAndBoard(UserEntity user, BoardEntity board) {
+        return heartRepository.findByUserAndBoard(user, board);
+    }
+
+    // 좋아요 생성
+    public HeartEntity addHeart(HeartEntity heartEntity) {
+        return heartRepository.save(heartEntity);
+    }
+
+    // 좋아요 삭제
+    public void deleteHeart(HeartEntity heart) {
+        heartRepository.delete(heart);
+    }
+}
