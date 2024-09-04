bc96b4140215a9e4d68f64d178d8d310a5fbfb87
tjdbwns19 <tjdbwns19@naver.com>
2024-09-04 12:54:00 +0900

    refactor: BoardBusiness -> deleteBoard 메서드 추가
    * deleteBoard: board 프록시 객체를 가져와 pathVariable과 비교 후 삭제

--- denstiny/main-server/src/main/java/com/example/domain/board/business/BoardBusiness.java
+++ denstiny/main-server/src/main/java/com/example/domain/board/business/BoardBusiness.java
@@ -7,8 +7,8 @@ import com.example.domain.board.service.BoardImageService;
 import com.example.domain.board.service.BoardService;
 import com.example.domain.board.converter.BoardConverter;
 import com.example.util.ImageUtil;
-import com.example.domain.board.controller.model.BoardRequest;
-import com.example.domain.board.controller.model.BoardResponse;
+import com.example.domain.board.controller.model.BoardAddRequest;
+import com.example.domain.board.controller.model.BoardAddResponse;
 import error.ErrorCode;
 import exception.ApiException;
 import lombok.AllArgsConstructor;
@@ -26,8 +26,7 @@ public class BoardBusiness {
     private final BoardImageService boardImageService;
     private final BoardConverter boardConverter;
 
-    public BoardResponse addBoard(BoardRequest boardReq, List<MultipartFile> images, Long userId){
-
+    public BoardAddResponse addBoard(BoardAddRequest boardReq, List<MultipartFile> images, Long userId){
         BoardEntity board = Optional.ofNullable(boardReq)
                 .map(it -> boardConverter.toBoardEntity(it, userId))
                 .map(it -> boardService.addBoard(it))
@@ -52,5 +51,19 @@ public class BoardBusiness {
         }
 
         return boardConverter.toBoardResponse(board);
+    }
+
+
+    public String deleteBoard(Long userId, Long boardId) {
+        // 1. getReferenceBoardId로 Board 엔티티 가져옴
+        BoardEntity board = boardService.getReferenceBoardId(boardId);
+
+        // 2. 조회한 board의 writer가 userId와 같은지 비교
+        if(board.getWriter().equals(userId)) {
+            // 3. writer와 userId가 같으면 게시글 삭제
+            boardService.deleteBoard(board);
+            return "성공적으로 유저" + userId + "이 게시글 번호" + boardId + "게시글을 삭제하였습니다.";
+        }
+        return "유저" + userId + "이 게시글 번호" + boardId + "게시글 삭제에 실패하였습니다.";
     }
 }
