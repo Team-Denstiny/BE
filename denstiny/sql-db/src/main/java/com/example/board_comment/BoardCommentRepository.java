c27f9fac23a4f4110e4a702091fa4558c7479f45
tjdbwns19 <tjdbwns19@naver.com>
2024-09-09 11:02:11 +0900

    refactor: 게시글 댓글 Repository 수정
    * Method - findBoardCommentByBoard: 게시글로 게시글 댓글 전체 조회

--- denstiny/sql-db/src/main/java/com/example/board_comment/BoardCommentRepository.java
+++ denstiny/sql-db/src/main/java/com/example/board_comment/BoardCommentRepository.java
@@ -1,6 +1,10 @@
 package com.example.board_comment;
 
+import com.example.board.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardCommentRepository extends JpaRepository<BoardCommentEntity, Long> {
+    List<BoardCommentEntity> findBoardCommentByBoard(BoardEntity board);
 }
